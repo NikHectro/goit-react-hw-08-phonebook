037810d0ca963a7f35ab773e3185ded5297bd0a7
NikHectro <99733904+NikHectro@users.noreply.github.com>
2022-10-17 17:33:43 +0300

    Add Loader

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -3,10 +3,12 @@ import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 // import { removeContact } from 'redux/actions';
 import { removeContact, fetchContacts } from 'redux/operations';
+import { Circles } from 'react-loader-spinner';
 
 export function ContactsList() {
   const contacts = useSelector(state => state.items.contacts);
   const filter = useSelector(state => state.filter);
+  const isLoading = useSelector(state => state.items.isLoading);
   const dispatch = useDispatch();
   // const contacts = useSelector(state => state.phoneBookReducer.contacts);
   // const filterInput = useSelector(state => state.phoneBookReducer.filter);
@@ -29,31 +31,36 @@ export function ContactsList() {
     // if (!contacts) {
     //   <h2>No contactcs</h2>
     // }
-    <ul>
-      {getContactsByFilter().map(contact => (
-        <li
-          key={contact.id}
-          style={{
-            padding: '10px',
-            margin: '0 auto',
-            width: '80vw',
-            gap: '40px',
-          }}
-        >
-          <span>
-            {contact.name}: {contact.number}
-          </span>
-          <button
-            style={{
-              padding: '5px',
-              marginLeft: '20px',
-            }}
-            onClick={() => deletedContactByID(contact.id)}
-          >
-            Delete
-          </button>
-        </li>
-      ))}
-    </ul>
+    <>
+      {isLoading && <Circles />}
+      {!isLoading && (
+        <ul>
+          {getContactsByFilter().map(contact => (
+            <li
+              key={contact.id}
+              style={{
+                padding: '10px',
+                margin: '0 auto',
+                width: '80vw',
+                gap: '40px',
+              }}
+            >
+              <span>
+                {contact.name}: {contact.number}
+              </span>
+              <button
+                style={{
+                  padding: '5px',
+                  marginLeft: '20px',
+                }}
+                onClick={() => deletedContactByID(contact.id)}
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 }
